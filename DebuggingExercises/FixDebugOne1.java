1d04bf11b417b57bb112c09fb5b00eaca388b482
Tan Beng Siang <bengsiang.tan@my.jcu.edu.au>
2022-11-18 16:43:38 +0800

    Debugged files

---
+++ DebuggingExercises/FixDebugOne1.java
@@ -0,0 +1,12 @@
+public class FixDebugOne1 {
+    /* This program displays a greeting */
+    public static void main(String[] args) {
+        //Systemoutprintl("Hello).
+      /*    the class was also missing opening and
+      closing curly braces at the start of the program.
+      the println statement has syntax errors.
+      there should be a dot in between each word.
+      java statements must always end with ; */
+        System.out.println("Hello");
+    }
+}
